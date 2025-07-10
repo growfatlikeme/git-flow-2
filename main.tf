7878a1b90197e8d8682e5b728e507d59a908ea6a
GrowFatLikeMe <freakishcrap@gmail.com>
2025-07-10 20:37:41 +0800

    Updated

--- main.tf
+++ main.tf
@@ -26,10 +26,6 @@ terraform {
 
 data "aws_caller_identity" "current" {}
 
-locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
-  account_id  = data.aws_caller_identity.current.account_id
-}
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "growfatfat-s3-tf-bkt"
